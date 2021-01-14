d8e83f2890edfe39bc4909d1a74d751543f341b8
do-ift <do@infinitech.dev>
2021-01-14 17:11:53 +0700

    initial react webpack

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react';
+
+// @ts-ignore
+// import a from './assets/images/a.png';
+import videoss from './vi.mp4'
+
+function App (){
+
+    useEffect(()=>{
+        const a = document.getElementById('#video')
+       
+    },[])
+    return (
+        <div className="">
+            <p className="abcA bcc">My Webpack</p>
+        </div>
+    )
+}
+
+export default App
---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options:{
+                            limit: 1024 * 1024,
+                            esModule: false,
+                        }
+                    },
+                ],
+            },
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                    }
+                ]
+            },{
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },{
+                test: /\.(mov|mp4)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            // favicon: 'public/favicon.ico'
+        })
+    ],
+};
